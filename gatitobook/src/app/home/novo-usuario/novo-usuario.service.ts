afd6f857ec364ba73c33c9e9ff96f2454f67f155
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-09-04 07:38:09 -0300

    service do componente novo-usuario

---
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,19 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario:NovoUsuario){
+  //novoUsuario recebe um objeto do tipo NovoUsuario que foi definido na interface
+  return this.http.post('http://localhost:3000/user/signup', novoUsuario);
+  //porta 3000 é onde está rodando a api, estamos mandando essa info para o backend
+  }
+}
+
+
