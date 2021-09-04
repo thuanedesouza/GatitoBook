6478aa042e53275f43ce2e0eedc9377bc7a70df0
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-09-04 08:25:05 -0300

    ReactiveForm de cadastro novo-usuario

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup; //usamos !: para mostrar que esse objeto pode ser nulo ou não
 
-  constructor() { }
+  constructor(
+    private FormBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.FormBuilder.group({
+      email:[''],
+      fullName:[''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
 }
