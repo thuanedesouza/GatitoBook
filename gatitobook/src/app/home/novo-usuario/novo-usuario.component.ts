e42a0f59540ade19b0effd5f7eacc8f1dd1e5f66
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-09-06 18:06:02 -0300

    adicionar novo user no backend

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -5,6 +5,7 @@ import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { UsuarioExisteService } from './usuario-existe.service';
 import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,7 +18,8 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private novoUsuarioService: NovoUsuarioService,
-    private usuarioExisteService: UsuarioExisteService
+    private usuarioExisteService: UsuarioExisteService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {    this.novoUsuarioForm = this.formBuilder.group(
@@ -35,7 +37,16 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if (this.novoUsuarioForm.valid){
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(()=>{
+        this.router.navigate([''])
+      }, (error)=>{ console.log(error)
+
+      })
+    }
+
+
+
   }
 }
