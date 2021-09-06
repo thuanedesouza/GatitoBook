50e79d9e668355ddd50fe6821c107bdd49fa8107
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-09-06 18:59:25 -0300

    token jwt

---
+++ gatitobook/src/app/autenticacao/token.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+//manipulando a API de local storage do chrome
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+// não injetamos nada nessa classe
+
+retornaToken(){
+  return localStorage.getItem(KEY) ?? '';// caso não pegue a chave retorna ''
+}
+
+salvaToken(token: string){
+  localStorage.setItem(KEY, token);
+}
+
+excluiToken(){
+  localStorage.removeItem(KEY);
+}
+
+possuiToken(){
+  return !!this.retornaToken;// !! verifica se tem ou não tem
+}
+
+}
